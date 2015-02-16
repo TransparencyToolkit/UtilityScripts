b673321ceb964489a1e43b50459300e3ac9760b5
M. C. McGrath <shidash@shidash.com>
2015-02-16 02:07:56 -0800

    Added LinkedIn scraper script and edited README

---
+++ linkedin.rb
@@ -0,0 +1,26 @@
+require 'json'
+require 'linkedindata'
+require 'fileutils'
+require 'json2csv'
+
+# Read JSON with search terms
+puts "Where is the list of terms to scrape?"
+scrapelist = gets.strip
+file = JSON.parse(File.read(scrapelist))
+
+# Create results dir
+puts "Where should results be saved?"
+resultsdir = gets.strip
+unless File.directory?(resultsdir)
+  Dir.mkdir(resultsdir)
+end
+
+# Go through all terms and scrape
+file.each do |term|
+  l = LinkedinData.new(term["Search Term"], term["Degrees"].to_i)
+  scrapeout = l.getData
+  filename = resultsdir+"/"+term["Search Term"].gsub(" ", "_")+".json"
+  File.write(filename, scrapeout)
+  File.write(filename.gsub(".json", ".csv"), `json2csv #{filename}`)
+end
+
