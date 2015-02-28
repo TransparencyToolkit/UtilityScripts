c135ac4ebe708cd1dc26bb5154b66fee95c18401
GH <shidash@shidash.com>
2015-02-28 02:07:04 +0100

    Updated install

--- config/install.sh
+++ config/install.sh
@@ -2,10 +2,7 @@
 
 apt-get install sudo tor elasticsearch gnumeric build-essential unzip rsync proxychains gnupg evince libreoffice emacs-nox curl make libssl-dev
 
-wget -P /home/gh https://www.torproject.org/dist/torbrowser/4.0.3/tor-browser-linux64-4.0.3_en-US.tar.xz
 cd /home/gh
-tar -xvJf tor-browser-linux64-4.0.3_en-US.tar.xz
-rm tor-browser-linux64-4.0.3_en-US.tar.xz
 mkdir data
 mkdir tools
 
@@ -18,5 +15,12 @@ rvm use 2.1.3
 gem install rails -v 4.1.7
 
 cd
+mkdir software
+cd software
 curl -L -O https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.4.zip
 unzip elasticsearch-1.3.4.zip
+rm elasticsearch-1.3.4.zip
+
+wget -P /home/gh/software https://www.torproject.org/dist/torbrowser/4.0.3/tor-browser-linux64-4.0.3_en-US.tar.xz
+tar -xvJf tor-browser-linux64-4.0.3_en-US.tar.xz
+rm tor-browser-linux64-4.0.3_en-US.tar.xz
