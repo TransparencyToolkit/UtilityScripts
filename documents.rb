8b09853386be0b3ba50aac08fff1a7f8e72daceb
Brennan Novak <bnvk@transparencytoolkit.org>
2016-05-28 21:11:28 +0200

    made Documents accept arg for local Tika server

--- documents.rb
+++ documents.rb
@@ -1,18 +1,27 @@
-require 'parsefile'
+require 'optparse'
+require 'ostruct'
 require 'dircrawl'
+require 'parsefile'
 
 class GrabLoadFile
 
-  def initialize(dir)
-    print "Loading directory: " + dir + "\n"
-    @dir = dir
+  def initialize(options, argv)
+	if argv[0]
+      puts "Loading directory: " + argv[0]
+      @dir = argv[0]
+	  @tika = options.tika
+	end
   end
 
   # Load in docs
   def run
+
+	puts "Using tika value"
+	puts @tika
+
     # Make blocks for dircrawl
-    block = lambda do |file, in_dir, out_dir|
-      p = ParseFile.new(file, in_dir, out_dir)
+    block = lambda do |file, in_dir, out_dir, tika|
+      p = ParseFile.new(file, in_dir, out_dir, tika)
       p.parse_file
     end
 
@@ -22,14 +31,21 @@ class GrabLoadFile
 
     # Call dircrawl
     out_dir = @dir+"_output"
-	#binding.pry
-    d = DirCrawl.new(@dir, out_dir, "_terms", false, block, include, @dir, out_dir)
+    d = DirCrawl.new(@dir, out_dir, "_terms", false, 
+                     block, include, @dir, out_dir, @tika)
     JSON.parse(d.get_output)
   end
 
 end
 
+
+# Arguments
+options = OpenStruct.new
+OptionParser.new do |opt|
+  opt.on('-t', '--tika TIKA', 'Use a local Tika server') { |o| options.tika = o }
+end.parse!
+
 puts "Grabbing some documents...."
 
-loadfile = GrabLoadFile.new(ARGV[0])
+loadfile = GrabLoadFile.new(options, ARGV)
 loadfile.run()
