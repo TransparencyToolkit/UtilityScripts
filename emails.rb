a7ad2e506e0d48e581e7a884b67480a307c3da8e
Debian Live user <shidash@shidash.com>
2016-08-15 15:44:47 +0000

    Updated dircrawl argument list in emails.rb

--- emails.rb
+++ emails.rb
@@ -11,7 +11,7 @@ class GrabLoadEmail
 
   # Load in docs
   def run
- 
+
     # Make blocks for dircrawl
     block = lambda do |file, in_dir, out_dir|
       p = Emailparser.new(file, out_dir, "attachments/")
@@ -19,19 +19,19 @@ class GrabLoadEmail
     end
 
     include = lambda do
-		require 'emailparser'
-	 end
+      require 'emailparser'
+    end
 
     # Call dircrawl
     out_dir = @dir+"_output"
 
-	# Create folder for attachments
-	extras = lambda do |out_dir|
-	  puts "created attachment directory"
+    # Create folder for attachments
+    extras = lambda do |out_dir|
+      puts "created attachment directory"
       Dir.mkdir(out_dir + "attachments") if !Dir.exist?(out_dir + "attachments")
-	end
+    end
 
-    d = DirCrawl.new(@dir, out_dir, "_terms", false, block, include, extras, @dir, out_dir)
+    d = DirCrawl.new(@dir, out_dir, "_terms", false, block, include, extras, "log", @dir, out_dir)
     JSON.parse(d.get_output)
   end
 
