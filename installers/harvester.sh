b5690829f7e905a3756740373cab0a398db87110
Brennan Novak <bnvk@transparencytoolkit.org>
2017-06-22 10:10:40 -0400

    tweaked various installer scripts

--- installers/harvester.sh
+++ installers/harvester.sh
@@ -3,8 +3,25 @@
 
 echo "Getting Debian dependencies"
 sudo apt-get install libcurl3 libcurl3-gnutls libcurl4-openssl-dev libmagickcore-dev libmagickwand-dev mongodb
+sleep 2
+
+echo "Adding Dotdeb repository and GPG key"
+echo "deb http://ftp.utexas.edu/dotdeb/ stable all \n deb-src http://ftp.utexas.edu/dotdeb/ stable all" | sudo tee /etc/apt/sources.list.d/dotdeb.list | cat
+wget -O $TT_APPS/dotdeb.gpg https://www.dotdeb.org/dotdeb.gpg
+sudo apt-key add dotdeb.gpg
+rm $TT_APPS/dotdeb.gpg
+sleep 2
+
+echo "Installing Redis"
+sudo apt-get update
+sudo apt-get install redis-server
+sleep 2
 
 echo "Getting Harvester codebase"
-git clone https://github.com/TransparencyToolkit/Harvester
-cd Harvester
+PATH_HARVESTER="$TT_APPS/Harvester/"
+git clone https://github.com/TransparencyToolkit/Harvester $PATH_HARVESTER
+cd $PATH_HARVESTER
 bundle install
+sleep 2
+
+echo "Done installing Harvester. Time to disco."
--- installers/tika.sh
+++ installers/tika.sh
@@ -2,31 +2,32 @@
 # Install Tika & Teseract
 LINE="-----------------------------------------------------------"
 
-echo "Installing Debian dependencies"
-sudo apt-get install default-jdk maven unzip
-echo $LINE
-sleep 1
-
-echo "Getting Tika source code"
-mkdir install
-curl https://codeload.github.com/apache/tika/zip/trunk -o trunk.zip
-unzip trunk.zip
-echo $LINE
-sleep 1
-
-echo "Installing Tika" 
-cd tika-trunk
-mvn -DskipTests=true clean install
-cp tika-server/target/tika-server-1.*-SNAPSHOT.jar /srv/tika-server-1.*-SNAPSHOT.jar
-echo $LINE
-sleep 1
+TIKA_VER="1.15"
+TIKA_ZIP="tika-1.15.zip"
 
 echo "Installing Tesseract via Debian"
 sudo apt-get -y -q install tesseract-ocr tesseract-ocr-deu tesseract-ocr-eng
-sleep 1
+sleep 2
 
-echo "Cleaning Up"
-rm trunk.zip
-sleep 1
+echo "Installing Tika dependencies via Debian"
+sudo apt-get install default-jdk maven unzip
+echo $LINE
+sleep 2
 
-echo "All done. Hooray!"
+echo "Getting Tika source code"
+curl -L https://github.com/apache/tika/archive/${TIKA_VER}.zip -o $TT_APPS/${TIKA_ZIP}
+unzip $TT_APPS/${TIKA_ZIP} -d $TT_APPS/
+rm $TT_APPS/${TIKA_ZIP}
+echo $LINE
+sleep 2
+
+echo "Installing Tika" 
+cd $TT_APPS/tika-${TIKA_VER}/
+mvn -DskipTests=true clean install
+
+echo "Copying Tika binary"
+echo sudo cp tika-server/target/tika-server-${TIKA_VER}-SNAPSHOT.jar /srv/tika-server-${TIKA_VER}-SNAPSHOT.jar
+echo $LINE
+sleep 2
+
+echo "Done installing Tika & Tesseract. Hooray!"
